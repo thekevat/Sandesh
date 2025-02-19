c25bf23d97b1fee068f6c42f3fa354be3d68ed5e
Rohit <kevatrohit3@gmail.com>
2025-02-19 22:50:06 +0530

    Initial commit

---
+++ server/Routes/UploadRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import multer from "multer";
+const router=express.Router();
+
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/images");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    },
+});
+
+const upload=multer({storage:storage});
+
+router.post('/',upload.single("file"),(req,res)=>{
+    try{
+       return res.status(200).json("File uploaded successfully");
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+export default router;
